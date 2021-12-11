7960c5cfbaba15d64075ace8264e9a219d298788
emmanuelcortes <emmanuel.cortes@gmail.com>
2021-12-11 04:15:51 -0300

    Adicionada a classe Livro

--- src/br/com/dio/PrimeiroIntellij.java
+++ src/br/com/dio/PrimeiroIntellij.java
@@ -1,12 +1,15 @@
 package br.com.dio;
 
 import br.com.dio.model.Gato;
+import br.com.dio.model.Livro;
 
 public class PrimeiroIntellij {
     public static void main(String [] args){
         System.out.println("Rodando Papai!");
         Gato gato = new Gato("gagau","black",9);
+        Livro livro = new Livro("O comendador", 200);
 
         System.out.println(gato);
+        System.out.println(livro);
     }
 }
